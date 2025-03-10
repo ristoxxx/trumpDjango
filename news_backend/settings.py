6536854af324aa1f848af55d365e3073141895cc
rlahteen <lahteri@gmail.com>
2025-03-10 17:07:42 +0200

    modified allowed hosts

--- news_backend/settings.py
+++ news_backend/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'trumpdjango-production.up.railway.app',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
